ee3756b02c1c49564846c3c952371c178d332bf7
Emre Vural <66080121+emrevural42@users.noreply.github.com>
2022-08-20 16:07:07 +0300

    Add files via upload

---
+++ console_application/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Sagopa Kajmer -NABER ");
+            Console.WriteLine("İsminizi Girin:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Girin:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba  "+ name + " " + surname);
+        }
+    }
+}
